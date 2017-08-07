485125f497a8052651ed9407568802233e1156b4
dyurovsky <dyurovsky@gmail.com>
2017-08-07 16:35:18 -0500

    receiver function

--- speaker_model.R
+++ speaker_model.R
@@ -8,3 +8,10 @@ outcomes %>%
   group_by(value) %>%
   summarise(n = n()) %>%
   mutate(n = n/sum(n))
+
+recall <- function(exposure) inv.logit(-.4252 + 1.1487*log2(exposure))
+
+
+recall2 <-(1-recall(1))*recall(1) + recall(1)
+
+(1-(recall2))*recall(1)+recall2
