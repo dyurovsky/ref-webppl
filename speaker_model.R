048acc4c3af697fce89043e1de2b9c622a00deb5
dyurovsky <dyurovsky@gmail.com>
2017-08-08 12:45:10 -0500

    working on possible vocabs

--- speaker_model.R
+++ speaker_model.R
@@ -1,7 +1,55 @@
 library(rwebppl)
 library(tidyverse)
 
+
+
+
+learn <- function(state, prob, count) {
+  
+  if(count == 0)
+    state
+  else {
+      
+    if(state)
+      new_state = state
+    else
+      new_state = as.logical(rbinom(1,1,prob))
+    
+    learn(new_state, prob, count -1)
+  
+  }
+}
+
+replicate(1000, learn(FALSE, .45, 4)) %>% mean
+
+outcomes <- webppl(program_file = "speaker.wppl")
+
+outcome_vals <- unique(outcomes$value) %>%
+  bind_rows(.id = "sample") %>%
+  group_by(exposures, label) %>%
+  summarise(known = mean(known))
+
+outcome_vals
+
+  spread(label, known) %>%
+  select(blicket, wug, dax)
+
+  unite(stringform, blicket:wug) %>%
+  distinct(stringform)
+
+outcomes <- webppl(program_file = "agent.wppl")
+
 outcomes <- webppl(program_file = "speaker.wppl") %>%
+  select(((ncol(.)/2)+1):ncol(.)) %>%
+  t() %>%
+  as_tibble() %>%
+  rename(point = V1, speak = V2) %>%
+  rowid_to_column(var = "sample") 
+
+hist(outcomes$speak)
+
+
+%>%
   as_data_frame
 
 outcomes %>% 
