3af78254794ce682942d52d3f949c0473abd69bb
CaLLab <benmorris@uchicago.edu>
2017-09-12 11:57:11 -0500

    broken speakerPlanner model

--- speaker_model.R
+++ speaker_model.R
@@ -5,20 +5,92 @@ library(ggplot2)
 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
+empiricalVocabs<- read.csv("empiricalVocabs.csv") %>% 
+  select(-X, -targetObjectName) %>%
+  rename(exposures = exposureRate,
+         label = realLabel,
+         known = performance)
+
+outcomes<-webppl(program_file = "speaker.wppl", data=empiricalVocabs, data_var = "empiricalVocabs")
+
+
+
+
+    
+
+
+## LearnPs analysis
+accs <- empiricalVocabs %>%
+  group_by(ldf_num) %>%
+  summarise(known = mean(known)) %>%
+  select(known)
+
+
+empirical_learn <-map(1:nrow(outcomes), function(row) as_tibble(outcomes[row,])) %>%
+  bind_rows() %>%
+  mutate(person = 1:nrow(.)) %>%
+  unnest() 
+
+empirical_mean <- empirical_learn %>%
+  group_by(person) %>%
+  summarise(p = weighted.mean(learnPs.support, learnPs.probs)) %>%
+  arrange(p)
+  
+ordered_learn <- empirical_learn %>%
+  mutate(person = factor(person, levels = empirical_mean$person))
+
+
+
+
+ggplot(ordered_learn, aes(x = learnPs.support, y = as.factor(person), 
+                              weight = learnPs.probs)) + 
+  geom_joy() + 
+  theme_bw()
+
+
+data_frame(value = rnorm(1000, 0, 40)) %>%
+  ggplot(aes(x = value)) + 
+  geom_histogram()
+
+
+quartz()
+hist(outcomes)
+
+
+
+
+
+
+# ------------------------------------------------------------------
+tmp <- outcomes %>%
+  select(((ncol(.)/2)+1):ncol(.)) %>%
+  t() %>%
+  as_tibble() %>%
+  rename(point = V1, speak = V2) %>%
+  rowid_to_column(var = "sample") %>% head()
+
+
+outcomes %>% group_by(ldf_num) %>% summarize(mean(value))
+#vs
+empiricalVocabs %>% group_by(ldf_num) %>% summarize(known=mean(known)) %>% filter(known<.3)
+
+
+
+
 
 learn <- function(state, prob, count) {
   
   if(count == 0)
     state
   else {
-      
+    
     if(state)
       new_state = state
     else
       new_state = as.logical(rbinom(1,1,prob))
     
     learn(new_state, prob, count -1)
-  
+    
   }
 }
 
@@ -36,10 +108,10 @@ outcome_vals <- outcomes$value %>%
 
 outcome_vals
 
-  spread(label, known) %>%
+spread(label, known) %>%
   select(blicket, wug, dax)
 
-  unite(stringform, blicket:wug) %>%
+unite(stringform, blicket:wug) %>%
   distinct(stringform)
 
 outcomes <- webppl(program_file = "agent.wppl")
@@ -52,92 +124,5 @@ outcomes <- webppl(program_file = "speaker.wppl") %>%
   rowid_to_column(var = "sample") 
 
 hist(outcomes$speak)
-
-empiricalTrials<- read.csv("empiricalTrials.csv")
-
-# trialsAsList = array(dim=length(unique((empiricalTrials$ldf))))
-# count=0
-# for (i in 1:max(empiricalTrials$ldf)+1) {
-#   trials = (empiricalTrials %>% filter(ldf_num==i) %>% select(realLabel))
-#   if(length(trials[[1]])==0) {
-#    print('skiiiiiiip meeeeeeeee') 
-#   }
-#   if(length(trials[[1]]) != 0) {
-#     trialsAsList[count] = trials
-#     count= count+1
-#   }
-#   if(count>198) {print(trials)}
-# }
-# 
-# outcomes<- NULL
-# for(i in 1:2){
-#   meIndex = (21 * (i-1))+1
-#   tmp=empiricalTrials[meIndex:(meIndex+20),]
-#   outcomes <- rbind(outcomes, cbind(i, webppl(program_file = "speaker.wppl", data=tmp, data_var="empiricalTrials")))
-# }
-    
-empiricalVocabs<- read.csv("empiricalVocabs.csv") %>% 
-  select(-X, -targetObjectName) %>%
-  rename(exposures = exposureRate,
-         label = realLabel,
-         known = performance)
-
-accs <- empiricalVocabs %>%
-  group_by(ldf_num) %>%
-  summarise(known = mean(known)) %>%
-  select(known)
-
-learnP
-
-
-
-
-
-outcomes<-webppl(program_file = "speaker.wppl", data=empiricalVocabs, data_var = "empiricalVocabs")
-
-
-empirical_learn <-map(1:nrow(outcomes), function(row) as_tibble(outcomes[row,])) %>%
-  bind_rows() %>%
-  mutate(person = 1:nrow(.)) %>%
-  unnest() 
-
-empirical_mean <- empirical_learn %>%
-  group_by(person) %>%
-  summarise(p = weighted.mean(learnPs.support, learnPs.probs)) %>%
-  arrange(p)
-  
-ordered_learn <- empirical_learn %>%
-  mutate(person = factor(person, levels = empirical_mean$person))
-
-
-#%>%
- 
-
-ggplot(ordered_learn, aes(x = learnPs.support, y = as.factor(person), 
-                              weight = learnPs.probs)) + 
-  geom_joy() + 
-  theme_bw()
-
-
-data_frame(value = rnorm(1000, 0, 40)) %>%
-  ggplot(aes(x = value)) + 
-  geom_histogram()
-
-
-quartz()
-hist(outcomes)
-
-
-tmp <- outcomes %>%
-  select(((ncol(.)/2)+1):ncol(.)) %>%
-  # t() %>%
-  as_tibble() %>%
-  rename(point = V1, speak = V2) %>%
-  rowid_to_column(var = "sample") %>% head()
-
-
-outcomes %>% group_by(ldf_num) %>% summarize(mean(value))
-#vs
-empiricalVocabs %>% group_by(ldf_num) %>% summarize(known=mean(known)) %>% filter(known<.3)
 
 
